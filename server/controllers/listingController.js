956ac5fb0cce7d142631fe04e8e79c65a52c7a04
bamche <blake.myrick@gmail.com>
2021-10-28 16:14:32 -0700

    initial controller for adding and fetching listings

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -2,20 +2,125 @@ const db = require('../models/listingModel');
 
 const listingController = {};
 
-listingController.generateUser = async (req, res, next) => {
-  const { email, name, teamName} = req.query;
+listingController.getListings = async (req, res, next) => {
+  const { lattitude, longitude, radius } = req.query;
+
+  //define box around search point, converting miles into degrees
+  // 69 miles = ~ 1 degree
+  const upperLat = lattitude + (radius/69);
+  const lowerLat = lattitude - (radius/69);
+  const upperLong = longitude + radius/(Math.abs(Math.cos(Math.PI*lattitude/180))*69);
+  const lowerLong = longitude - radius/(Math.abs(Math.cos(Math.PI*lattitude/180))*69);
 
   try{
-    const SQLQueryString = `INSERT INTO public.user_list (user_name, email, team_name) VALUES ('${name}', '${email}', '${teamName}');`;
-    await db.query(SQLQueryString);
+    //select all columns from listings where query location is between the upper and lower bounds
+    const listingQueryString = `
+      SELECT * FROM public.Listings
+      WHERE Listings.longitude between '${upperLong}' and '${lowerLong}'
+      and Listings.lattitude between '${upperLat}' and '${lowerLat}';`;
+    const listings = await db.query(listingQueryString);
+
+    //need to perform and inner join here to get discussion, picutes, etc
+    const listingID = listings
+
+    res.locals.listings = listings;
     
     return next();
-} catch(err){
-  return next({
-    log: 'SQLController.generateUser: ERROR: Error adding user',
-    message: { err: `Error occurred in SQLController.generateUser. err log: ${err}` }
-  });
-}
+  } catch(err){
+    return next({
+      log: 'listingController.getListings: ERROR: Error fethcing listings',
+      message: { err: `Error occurred in listingController.getListings. err log: ${err}` }
+    });
+  }
 };
 
+listingController.postListing = async (req, res, next) => {
+
+  const { 
+    id,
+    title, 
+    description, 
+    street_address, 
+    city, 
+    state, 
+    lattitude, 
+    longitude, 
+    upvote, 
+    posted_by 
+  } = req.body.listing;
+
+  //get string for values to add to image table
+  const imageInfo = res.locals.imageInfo;
+
+  try{
+    const listingQueryString = 
+      `INSERT INTO public.Listings 
+      (id, title, description, street_address, city, state, 
+      lattitude, longitude, upvote, posted_by) 
+      VALUES (
+        '${id}',
+        '${title}',  
+        '${description}', 
+        '${street_address}', 
+        '${city}', 
+        '${state}', 
+        '${lattitude}', 
+        '${longitude}', 
+        '${upvote}', 
+        '${posted_by}'
+        );`;
+
+    const imageQueryString = 
+      `INSERT INTO public.Images 
+      (id, description, listing_id, url) 
+      VALUES ${imageInfo};`;
+    
+
+    const listings = await db.query(listingQueryString)
+    await db.query(imageQueryString);
+    res.locals.listings = listings;
+    
+    return next();
+  } catch(err){
+    return next({
+      log: 'listingController.postListing: ERROR: Error posting a listing',
+      message: { err: `Error occurred in listingController.postListing. err log: ${err}` }
+    });
+  }
+};
+
+listingController.getOneListing = async (req, res, next) => {
+  const { listing_id } = req.query;
+
+  try{
+    //select all columns from specified listing based on id
+    //////////POTENTIAL TO OPTIMIZE BY NOT REFETCHING DATA 
+    const listingQueryString = `
+      SELECT * FROM public.Listings
+      WHERE Listings.id=${listing_id};`;
+
+    const imageQueryString = `
+      SELECT * FROM public.Images
+      WHERE Images.listing_id=${listing_id};`;
+      
+    const discussionQueryString = `
+      SELECT * FROM public.Discussions
+      WHERE Discussions.listing_id=${listing_id};`;
+      
+    const listing = await db.query(listingQueryString);
+    const images = await db.query(imageQueryString);
+    const discussions = await db.query(discussionQueryString);
+
+    res.locals.listings = listings;
+    res.locals.images = images;
+    res.locals.discussions = discussions;
+    
+    return next();
+  } catch(err){
+    return next({
+      log: 'listingController.getOneListing: ERROR: Error fethcing listing',
+      message: { err: `Error occurred in listingController.getOneListing. err log: ${err}` }
+    });
+  }
+};
 module.exports = listingController;
