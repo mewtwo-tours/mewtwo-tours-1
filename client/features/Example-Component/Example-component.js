2a64acf4897a4e0d56d2ac37e5c05e350497b0fb
adam <blackwell.ada@gmail.com>
2021-10-27 21:08:05 -0400

    added client file structure, basic store

---
+++ client/features/Example-Component/Example-component.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, TextInput } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux'
+//import {whatever} from './exampleSlice.js
+
+const Example_component = () => {
+  
+}
