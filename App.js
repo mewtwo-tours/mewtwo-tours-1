761555c2b2fab9eb6cec601621a91cf07297c506
Murphypie <ochemypie@gmail.com>
2021-10-29 22:59:07 -0400

    MapView Preliminary

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, Form, Input } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, Form, Input, Dimensions } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
 import MainView from './client/features/main-view/MainView'
 import { Provider } from 'react-redux';
 import store from './client/store/store';
 
 
 export default function App() {
+
+  // --------------- MapView stuff ----------------- //
+  const [currlocation, setCurrLocation] = useState(null);
+    useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      await setCurrLocation(location);
+    })();
+  }, []);
+
+  //console.log(currlocation)
+  // let mapCompo = currlocation === null ? 
+  // <Text>Open up App.js to start working on your app!</Text>
+  // : <MapView style={styles.map} showsUserLocation={true}> 
+  // <MapView.Marker
+  //         coordinate={{ latitude: currlocation.coords.latitude, longitude: currlocation.coords.longitude}}
+  //         title={'Pin'}
+  //         description={`${currlocation}`}/>
+  // </MapView> 
+
+
+
+
   // --------------- GeoCode snippet --------------- //
   const messageObj = {
     location:  '153 Morgan Ave, Brooklyn, NY 11237'
@@ -28,22 +58,24 @@ export default function App() {
   }
   // ------------------------------------------------- //
   
+
+
   // --------------- Amazon s3 snippet --------------- //
   const [newImg, setNewImg] = useState("");
   const [images, setImages] = useState([]);
   const [description, setDescription] = useState('');
   const [file, setFile] = useState();
 
-  useEffect(() => {
-    (async () => {
-    //  if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
-        }
-      //}
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     if (Platform.OS !== 'web') {
+  //       const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  //       if (status !== 'granted') {
+  //         alert('Sorry, we need camera roll permissions to make this work!');
+  //       }
+  //     }
+  //   })();
+  // }, []);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -92,12 +124,19 @@ export default function App() {
     // ------------------------------------------------- //
 
   return (
-    // <Provider store = {store}>
-    //   <SafeAreaView style={styles.container}> 
-    //     <MainView> 
-    //     </MainView>
-    //   </SafeAreaView>
-    // </Provider>
+    <Provider store = {store}>
+      <SafeAreaView style={styles.container}> 
+        <MainView> 
+        </MainView>
+      </SafeAreaView>
+    </Provider>
+
+
+    // ------ MapView -------- //
+
+    // <View style={styles.container}>
+    //   {mapCompo}
+    // </View>
 
 
 
@@ -115,22 +154,22 @@ export default function App() {
 
     // ------ Amazon s3 -------- //
 
-    <View style={styles.container}>
-      {/* <Form  //onSubmit={handleSubmit}
-      > */}
-      {/* <Input
-        type="file"
-        accept="image/*"
-        //onPress={postImage}
-      ></Input>
+    // <View style={styles.container}>
+    //   <Form  //onSubmit={handleSubmit}
+    //   >
+    //   <Input
+    //     type="file"
+    //     accept="image/*"
+    //     //onPress={postImage}
+    //   ></Input>
       
-      <Input
-        //onPress={(e) => setDescription(e.target.value)}
-        type="text"
-      ></Input> */}
-      <Button></Button>
-    {/* </Form> */}
-    </View>
+    //   <Input
+    //     //onPress={(e) => setDescription(e.target.value)}
+    //     type="text"
+    //   ></Input>
+    //   <Button></Button>
+    // </Form>
+    // </View>
 
   );
 }
@@ -141,5 +180,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
